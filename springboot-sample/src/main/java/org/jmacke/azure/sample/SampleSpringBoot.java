6e7aaf1600289d3ce722aa27898f9ae4b1a09e6a
Jonathan Macke <jonathan.macke@yahoo.fr>
2017-01-13 15:09:15 +0100

    change config to generate a war file instead of a jar

--- springboot-sample/src/main/java/org/jmacke/azure/sample/SampleSpringBoot.java
+++ springboot-sample/src/main/java/org/jmacke/azure/sample/SampleSpringBoot.java
@@ -2,10 +2,19 @@ package org.jmacke.azure.sample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SampleSpringBoot {
+public class SampleSpringBoot extends SpringBootServletInitializer { 
 
+	
+	@Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(SampleSpringBoot.class);
+    }
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SampleSpringBoot.class, args);
 	}
